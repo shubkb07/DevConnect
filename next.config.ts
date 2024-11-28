7f8fbdb862c9c6ec52f271c1d22b8b29d68088e0
Shubham Kumar Bansal <shub@shubkb.com>
2024-11-28 10:28:14 +0530

    Update: git ignore

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,10 @@
 import type { NextConfig } from "next";
+import { config } from 'dotenv';
+
+// Load .env file only in non-production environments
+if (process.env.NODE_ENV !== 'production') {
+  config();
+}
 
 const nextConfig: NextConfig = {
   /* config options here */
